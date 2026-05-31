b3c1b1a26e747441ca71cf161a221bed7aaf84c3
Viktor (Makflay) <chief.makflay1@gmail.com>
2026-05-31 12:00:52 +0300

    feat: add dto validation

--- libs/shared/src/dto/event.dto.ts
+++ libs/shared/src/dto/event.dto.ts
@@ -1,6 +1,15 @@
-export type EventDto = {
+import { IsDateString, IsObject, IsString, IsUUID } from 'class-validator';
+
+export class EventDto {
+  @IsUUID()
   id: string;
+
+  @IsString()
   type: string;
+
+  @IsObject()
   payload: Record<string, unknown>;
+
+  @IsDateString()
   createdAt: string;
-};
+}
